28895f8251f6d6ba764aae673a81d6c3857b4f76
Frechman <31288209+Frechman@users.noreply.github.com>
2017-11-30 16:38:52 +0300

    Task 1.3.  JavaDock Code convention Add packege-info, modified directory

---
+++ chapter_001/src/main/java/ru/frechman/Calculate.java
@@ -0,0 +1,18 @@
+package ru.frechman;
+/*
+ *Class Calculate решение задачи части 001 урок 1.
+ *@author gavrilov
+ *@since 30.11.2017
+ */
+public class Calculate {
+    
+    /*
+     *Метод main запускает программу и выводит на экран "Hello World!"
+     *@param args - Параметры метода main, можно передать аргументы, например, из command line .
+     */
+    public static void main(String... args) {
+        
+        System.out.println("Hello World!");
+    
+    }
+}
