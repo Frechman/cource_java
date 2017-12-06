d1f312749e9ed09956d739f31c888117484455ad
Frechman <31288209+Frechman@users.noreply.github.com>
2017-12-06 12:46:54 +0300

    4.1. Подсчет суммы чётных чисел в диапазоне. Add classes Counter and CounterTest.

---
+++ chapter_001/src/main/java/ru/frechman/loop/Counter.java
@@ -0,0 +1,27 @@
+package ru.frechman.loop;
+
+/**
+ * Cчётчик.
+ *
+ * @author Frechman.
+ * @since 0.1 
+ * @version 0.1.
+ */
+public class Counter {
+
+    /**
+     * Считает сумму чётных чисел в заданном диапазоне.
+     * @param start Начало диапазона.
+     * @param finish Окончание диапазона. 
+     * @return Сумму чётных чисел.
+     */
+    public int add(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            if (i % 2 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
---
+++ chapter_001/src/test/java/ru/frechman/loop/CounterTest.java
@@ -0,0 +1,39 @@
+package ru.frechman.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Тест класса Counter.
+ */
+public class CounterTest {
+
+    @Test
+    public void testAdd() {
+        Counter count = new Counter();
+        int start = -4;
+        int finish = 10;
+
+        int actualResult = count.add(start, finish);
+        int expected = 24;
+
+        assertThat(actualResult, is(expected));
+    }
+
+    @Test
+    public void testAddFromOneToTen() {
+        Counter count = new Counter();
+        int start = 1;
+        int finish = 10;
+
+        int actualResult = count.add(start, finish);
+        int expected = 30;
+
+        assertThat(actualResult, is(expected));
+
+        assertEquals(0, count.add(-30, 30));
+        assertEquals(2550, count.add(0, 100));
+        assertEquals(2548, count.add(-2, 100));
+    }
+}
