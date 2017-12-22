5b4c27cf6d78bd1d754b4593ff4cfbd1899aab6e
Frechman <31288209+Frechman@users.noreply.github.com>
2017-12-22 15:05:40 +0300

    P2 6.2. Рефакторинг - Шаблон Декоратор для валидатора. Refactor ValidateInput and add tests.

--- chapter_002/Tracker/src/main/java/ru/frechman/start/ValidateInput.java
+++ chapter_002/Tracker/src/main/java/ru/frechman/start/ValidateInput.java
@@ -1,6 +1,17 @@
 package ru.frechman.start;
 
-public class ValidateInput extends ConsoleInput {
+public class ValidateInput implements Input {
+
+    private Input input;
+
+    ValidateInput(final Input input) {
+        this.input = input;
+    }
+
+    @Override
+    public String ask(String question) {
+        return this.input.ask(question);
+    }
 
     @Override
     public int ask(String question, int[] range) {
@@ -8,7 +19,7 @@ public class ValidateInput extends ConsoleInput {
         int value = -1;
         do {
             try {
-                value = super.ask(question, range);
+                value = this.input.ask(question, range);
                 invalid = false;
             } catch (MenuOutException moe) {
                 System.out.println("Please, enter item from menu.");
