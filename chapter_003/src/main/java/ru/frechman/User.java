b8874b3c2a3a3be368e1b5d21a367e4c848346a4
Frechman <31288209+Frechman@users.noreply.github.com>
2018-01-16 22:37:24 +0300

    P3 2.2. Написать программу преобразования List в Map. Add method Convert List to Map.

---
+++ chapter_003/src/main/java/ru/frechman/User.java
@@ -0,0 +1,32 @@
+package ru.frechman;
+
+public class User {
+
+
+    private Integer id;
+
+    private String name;
+
+    private String city;
+
+    public User() {
+    }
+
+    public User(Integer id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+}
---
+++ chapter_003/src/main/java/ru/frechman/UserConvert.java
@@ -0,0 +1,28 @@
+package ru.frechman;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Convert List to Map.
+ */
+public class UserConvert {
+
+
+    /**
+     * Convert List to Map.
+     *
+     * @param list the List.
+     * @return Map, where key is User.id, and value is User.
+     */
+    public HashMap<Integer, User> process(List<User> list) {
+
+        HashMap<Integer, User> mapUsers = new HashMap<>();
+        for (User current : list) {
+            mapUsers.put(current.getId(), current);
+        }
+
+        return mapUsers;
+    }
+
+}
