1e11b4c40d843d8a5b4de9e8f69719e9b06f6d6e
Frechman <31288209+Frechman@users.noreply.github.com>
2018-01-17 12:15:47 +0300

    P3 2.2. Small edit style code.

--- chapter_003/src/main/java/ru/frechman/lite/ListExample.java
+++ chapter_003/src/main/java/ru/frechman/lite/ListExample.java
@@ -1,10 +1,15 @@
 package ru.frechman.lite;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
 
 public class ListExample {
 
     static class User {
+
         private final String name;
 
         private User(String name) {
@@ -13,8 +18,12 @@ public class ListExample {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
             User user = (User) o;
             return Objects.equals(name, user.name);
         }
@@ -27,9 +36,9 @@ public class ListExample {
 
         @Override
         public String toString() {
-            return "User{" +
-                    "name='" + name + '\'' +
-                    '}';
+            return "User{"
+                    + "name='" + name + '\''
+                    + '}';
         }
 
         public int getInt(int max) {
@@ -38,9 +47,9 @@ public class ListExample {
 
     }
 
-    static class userPet extends User {
+    static class UserPet extends User {
 
-        private userPet(String name) {
+        private UserPet(String name) {
             super(name);
         }
     }
@@ -50,7 +59,6 @@ public class ListExample {
         List<User> users = new ArrayList<>();
 
         users.addAll(Arrays.asList(new User("pert"), new User("serj")));
-
 
         Iterator<User> iterator = users.iterator();
 
