b5033c411b3ee1d0fcd86d5a560e1376fdb5752a
Frechman <31288209+Frechman@users.noreply.github.com>
2018-01-31 11:28:03 +0300

    P2 Tack CoffeeMachine.

--- chapter_002/Task/src/main/java/task/CoffeeMachine.java
+++ chapter_002/Task/src/main/java/task/CoffeeMachine.java
@@ -1,25 +1,39 @@
 package task;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CoffeeMachine {
+
+    private final static int[] COINS = {100, 50, 10, 5, 2, 1};
 
     /**
      * в автомате монеты наминалом 1 2 5 10 Пример. Мы засунули 50 рублей выбрали кофе за 35. Сдачу
      * автомат должен дать 15 рублей Алгоритм должен вернуть наименьшее количество монет. Метод
      * вернет массив {10, 5} = 15 рублей
      *
-     * @param value купюра 50,100...
-     * @param price цена коффе
+     * @param value banknote.
+     * @param price price of coffee.
+     * @return change.
      */
-    public int[] changes(int value, int price) {
-        int change = price - value;
-        int[] result = new int[change];
+    public List<Integer> change(int value, int price) {
+        int amount = value - price;
+        List<Integer> change = new ArrayList<>();
 
-        for (int i = 0; i < result.length; i++) {
-            if (change % 10 == 0) {
-                result[i] = change % 10;
+        for (int i = 0; i < COINS.length; i++) {
+            while (amount >= COINS[i]) {
+                change.add(COINS[i]);
+                amount -= COINS[i];
             }
         }
 
-        return result;
+        return change;
+        /*int[] result = new int[change.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = change.get(i);
+        }
+        return result;*/
+
+        //return change.stream().mapToInt(i-> i).toArray();
     }
 }
--- chapter_002/Task/src/test/java/task/CoffeeMachineTest.java
+++ chapter_002/Task/src/test/java/task/CoffeeMachineTest.java
@@ -1,14 +1,100 @@
 package task;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
 public class CoffeeMachineTest {
 
-/*    @Test
+    private CoffeeMachine coffeeMachine = new CoffeeMachine();
+
+    List<Integer> coins(Integer... ints) {
+        return new ArrayList<>(Arrays.asList(ints));
+    }
+
+    @Test
+    public void changeFor1() {
+        assertEquals(coins(1), coffeeMachine.change(2, 1));
+    }
+
+    @Test
+    public void changeFor2() {
+        assertEquals(coins(2), coffeeMachine.change(4, 2));
+    }
+
+    @Test
+    public void changeFor3() {
+        assertEquals(coins(2, 1), coffeeMachine.change(50, 47));
+    }
+
+    @Test
+    public void changeFor4() {
+        assertEquals(coins(2, 2), coffeeMachine.change(50, 46));
+    }
+
+    @Test
+    public void changeFor5() {
+        assertEquals(coins(5), coffeeMachine.change(50, 45));
+    }
+
+    @Test
+    public void changeFor7() {
+        assertEquals(coins(5, 2), coffeeMachine.change(100, 93));
+    }
+
+    @Test
+    public void changeFor9() {
+        assertEquals(coins(5, 2, 2), coffeeMachine.change(50, 41));
+    }
+
+    @Test
+    public void changeFor10() {
+        assertEquals(coins(10), coffeeMachine.change(50, 40));
+    }
+
+    @Test
+    public void changeFor13() {
+        assertEquals(coins(10, 2, 1), coffeeMachine.change(50, 37));
+    }
+
+    @Test
+    public void changeFor16() {
+        assertEquals(coins(10, 5, 1), coffeeMachine.change(50, 34));
+    }
+
+    @Test
+    public void changeFor23() {
+        assertEquals(coins(10, 10, 2, 1), coffeeMachine.change(50, 27));
+    }
+
+    @Test
+    public void changeFor38() {
+        assertEquals(coins(10, 10, 10, 5, 2, 1), coffeeMachine.change(100, 62));
+    }
+
+    @Test
+    public void changeFor189() {
+        assertEquals(coins(100, 50, 10, 10, 10, 5, 2, 2), coffeeMachine.change(500, 311));
+    }
+
+    @Test
+    public void banknote50price35changeFor15() {
+        assertEquals(coins(10, 5), coffeeMachine.change(50, 35));
+    }
+
+    /*
+    //for return int[]
+    @Test
     public void changes() {
-        CoffeeMachine coffeeMachine = new CoffeeMachine();
-
-        int price = 50;
+        int value = 50;
+        int price = 35;
         int[] expected = {10, 5};
+        int[] actual = coffeeMachine.change(value, price);
+        assertArrayEquals(expected, actual);
+    }
+    */
 
-        assertThat(coffeeMachine.changes(35, price), is(expected));
-    }*/
 }
