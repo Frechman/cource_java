d938530a52d055c7f072f5fc1c03799e97932351
dev <serj.g13@yandex.ru>
2018-08-27 16:53:27 +0300

    Fix task Filters

--- chapter_006/src/main/resources/sql/filtres.sql
+++ chapter_006/src/main/resources/sql/filtres.sql
@@ -1,15 +1,52 @@
+CREATE DATABASE product_db;
+
 CREATE TABLE Product (
-  id           INT,
+  id           SERIAL PRIMARY KEY,
   name         VARCHAR,
   type_id      INT,
   expired_date DATE,
   price        INT
 );
 CREATE TABLE Type (
-  id   INT,
+  id   SERIAL PRIMARY KEY,
   name VARCHAR
 );
+-- data for tests
+INSERT INTO Product (id, name, type_id, expired_date, price)
+VALUES (1, '1', 1, '2018-01-01', 10);
+INSERT INTO Product (id, name, type_id, expired_date, price)
+VALUES (2, '123', 2, '2018-01-01', 20);
+INSERT INTO Product (id, name, type_id, expired_date, price)
+VALUES (3, '1234', 2, '2018-09-01', 30);
+INSERT INTO Product (id, name, type_id, expired_date, price)
+VALUES (4, '1234', 2, '2018-01-01', 40);
+INSERT INTO Product (id, name, type_id, expired_date, price)
+VALUES (5, '1', 3, '2018-09-01', 50);
+INSERT INTO Product (id, name, type_id, expired_date, price)
+VALUES (16, '1423', 3, '2018-09-30', 60);
+INSERT INTO Product (id, name, type_id, expired_date, price)
+VALUES (71, 'мороженное', 3, '2018-09-11', 70);
+INSERT INTO Product (id, name, type_id, expired_date, price)
+VALUES (8, '1', 2, '2018-01-01', 80);
+INSERT INTO Product (id, name, type_id, expired_date, price)
+VALUES (191, 'мороженноеЫВА', 2, '2018-01-01', 90);
+INSERT INTO Product (id, name, type_id, expired_date, price)
+VALUES (10, 'ЫВА МОРОЖЕННОЕ ЫВА', 1, '2018-01-01', 100);
+INSERT INTO Product (id, name, type_id, expired_date, price)
+VALUES (11, '1', 1, '2018-01-01', 110);
+INSERT INTO Product (id, name, type_id, expired_date, price)
+VALUES (12, '1', 1, '2018-01-01', 110);
 
+INSERT INTO Type (id, name)
+VALUES (1, 'СЫР');
+INSERT INTO Type (id, name)
+VALUES (2, 'МОЛОКО');
+INSERT INTO Type (id, name)
+VALUES (3, 'МОРОЖЕННОЕ');
+
+SELECT *
+FROM Product;
+-- Tasks:
 -- 1. Написать запрос получение всех продуктов с типом "СЫР"
 SELECT P.*
 FROM Product AS P
@@ -24,27 +61,27 @@ WHERE name LIKE '%мороженное%';
 -- 3. Написать запрос, который выводит все продукты, срок годности которых заканчивается в следующем месяце.
 SELECT *
 FROM Product
-WHERE expired_date
-          BETWEEN current_timestamp + INTERVAL '1 mons'
-          AND current_timestamp + INTERVAL '2 mons';
+WHERE extract(MONS FROM expired_date :: date)
+          BETWEEN extract(MONS FROM current_timestamp :: date) + 1
+          AND extract(MONS FROM current_timestamp :: date) + 2;
 
 -- 4. Написать запрос, который выводит самый дорогой продукт.
 SELECT *
 FROM Product
-WHERE MAX(price);
+WHERE price = (SELECT MAX(price) FROM Product);
 
 -- 5. Написать запрос, который выводит количество всех продуктов определенного типа.
 SELECT count(*)
 FROM Product
 WHERE type_id = 2;
 --OR
-SELECT count(*)
+SELECT count(1)
 FROM Product AS P
        JOIN Type AS T ON P.type_id = T.id
 WHERE T.name = 'СЫР';
 
 -- 6. Написать запрос получение всех продуктов с типом "СЫР" и "МОЛОКО"
-SELECT P.*
+SELECT *
 FROM Product AS P
        JOIN Type AS T ON P.type_id = T.id
 WHERE T.name IN ('СЫР', 'МОЛОКО');
@@ -53,16 +90,13 @@ SELECT *
 FROM Product AS P
        JOIN Type AS T ON P.type_id = T.id
 WHERE T.name = 'СЫР'
-   OR 'МОЛОКО';
+   OR T.name = 'МОЛОКО';
 
 -- 7. Написать запрос, который выводит тип продуктов, которых осталось меньше 10 штук.
 SELECT P.type_id
 FROM Product AS P
-WHERE (SELECT count(*) FROM Product GROUP BY type_id) < 10;
---OR
-SELECT T.name
-FROM Product AS P
-       JOIN (SELECT * FROM Type GROUP BY name HAVING count(name) < 10) AS T ON P.type_id = T.id;
+GROUP BY type_id
+HAVING count(*) < 10;
 
 --     8. Вывести все продукты и их тип.
 SELECT P.name, T.name
