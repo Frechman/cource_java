7e9e1ad06e57158beca85acf97f6d6c62eb04c0f
Frechman <31288209+Frechman@users.noreply.github.com>
2018-09-06 15:57:50 +0300

    1. Создать web проект [#2514]

---
+++ chapter_007/task_web_project/src/main/java/ru/frechman/servlets/EchoServlet.java
@@ -0,0 +1,18 @@
+package ru.frechman.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class EchoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html; charset=utf-8");
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello World!");
+    }
+
+}
